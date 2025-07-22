8952bfa0b2929b9ba6e5a3dadec71c43fa316c0a
Joaquin Castaño Trujillo <148889113+Joaquin9999@users.noreply.github.com>
2025-07-22 08:58:18 -0500

    Reto v1

---
+++ Reto/anim/script.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+  gsap.to("#nube", { x: 600, duration: 8, repeat: -1, yoyo: true, ease: "power1.inOut" });
+  gsap.to("#sol", { rotation: 360, transformOrigin: "50% 50%", duration: 10, repeat: -1, ease: "linear" });
+
+  const bici = document.getElementById("bici");
+
+  const tl = gsap.timeline({ repeat: -1 });
+  tl.to(bici, { x: -200, duration: 2, ease: "power1.inOut" }) 
+    .to(bici, { x: 200, duration: 4, ease: "power1.inOut" })  
+    .to(bici, { x: -200, duration: 4, ease: "power1.inOut" }); 
+};
+
+const biciSonido = new Howl({
+  src: ["Reto/anim/Sonido/Sonido de Bicicleta Pedalenado.mp3"], 
+  volume: 0.5
+});
+
+document.getElementById("bici").addEventListener("click", function() {
+  biciSonido.play();
+}); 
+
